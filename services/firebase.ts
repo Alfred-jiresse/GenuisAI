39b4d47c221f8a7888344cb460260b70d42f0078
Alfred-jiresse <jiressealfred067@gmail.com>
2025-11-29 02:03:00 -0500

    change somthing

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,27 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.ts
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB8PHjRMln_s_znJwdctGQm5fZUV71OefI",
   authDomain: "genuisai-b4574.firebaseapp.com",
   projectId: "genuisai-b4574",
-  storageBucket: "genuisai-b4574.firebasestorage.app",
+  storageBucket: "genuisai-b4574.appspot.com",
   messagingSenderId: "355181203722",
-  appId: "1:355181203722:web:9e75c3cc14f64a6b7baaf3",
-  measurementId: "G-HX6W1YYZNZ"
+  appId: "1:355181203722:web:9e75c3cc14f64a6b7baaf3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
+// Empêche Firebase de s'initialiser deux fois
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export { app, analytics, db, auth };
+// Exporte les services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
